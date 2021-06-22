e10be782ebe464cf8eea5c3257394f0cabb19663
Areeba Khan <kahnareeba@gmail.com>
2021-06-22 18:10:51 -0500

    code can now create the psuedo .tif files in their appropriate replicate_# folder

--- moving_tifs.cpp
+++ moving_tifs.cpp
@@ -25,6 +25,27 @@ void print_vector(vector<string> v) {
     }
     cout << endl;
 }
+
+string add_zeros(string s){
+    int len = s.length();
+    for (int i = 0; i < (3 - (len % 3)) % 3 ; i++) 
+        s = "0" + s;    
+    return s;
+}
+
+string create_filename(string n){
+    // where n is a number, i.e 1, 3, 5..
+    string filename, prefix, suffix;
+    
+    suffix = to_string(stoi(n) + 1);
+
+    prefix = add_zeros(n);
+    suffix = add_zeros(suffix) + ".tif";
+    filename = prefix + "-" + suffix;    
+    
+    // now to do the same with 
+    return filename;
+}
   
 void create(vector<vector<string>> files){
     /* creates replicate_# directories and "fake" .tiff text files */
@@ -50,17 +71,31 @@ void create(vector<vector<string>> files){
     // takes int and creates a file with it inside each replicant
     // the content of the files is n, 6, or 8 
 
-    // cout << "directories: ";   
-    // print_vector(directories);
+    cout << "directories: ";   
+    print_vector(directories);
 
-    for (int dir = 0; dir < directories.size(); dir++){
-        ofstream tif;
-        tif.open;
+    for (int row = 1; row < files.size(); row++){
+        string file_int = files.at(row).at(0); 
+        for (int dir = 0; dir < directories.size(); dir++){
+            string file_content = files.at(row).at(dir + 1);
+            if (!file_content.empty()) {
+                string filename = directories.at(dir) + "/";
+
+                filename = filename + create_filename(file_int);
+
+                ofstream tif;
+                tif.open(filename);
+                tif << file_content << "\n";
+                tif.close();
+            }
+        }
+
     }
-
 }
 
 int main() {  
+    // string num = "1";  // test code fo create_filename func.
+    // cout << "filename: " << create_filename(num) << endl;
 
     ifstream f("06.11.2021_DG5248_GFP_OLLAS_MSP.csv");
 
@@ -91,9 +126,9 @@ int main() {
         cout << "file did not open" << endl;
     }
 
-    for(int i = 0; i < csv.size(); i++){  /* prints out all values in csv file */
-        print_vector(csv.at(i));
-    }
+    // for(int i = 0; i < csv.size(); i++){  /* prints out all values in csv file */
+    //     print_vector(csv.at(i));
+    // }
     
     create(csv);
 
