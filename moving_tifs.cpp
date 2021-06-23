aacbb58cc0d9f71676f4d18108a5c73ddecebee8
Areeba Khan <kahnareeba@gmail.com>
2021-06-22 20:05:41 -0500

    remove duplicate moving_tiffs.* and adding the sorting function to moving_tifs.cpp

--- moving_tifs.cpp
+++ moving_tifs.cpp
@@ -5,6 +5,7 @@
 
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <experimental/filesystem>
 
 #include <string>
 #include <vector>
@@ -48,7 +49,8 @@ string create_filename(string n){
 }
   
 void create(vector<vector<string>> files){
-    /* creates replicate_# directories and "fake" .tiff text files */
+    /* creates replicate_# directories and "fake" .tiff text files -
+        for the sake of testing this code on smaller tif files.  */
 
     vector<string> directories;     /* holds the strings to replicate_#*/
 
@@ -60,19 +62,18 @@ void create(vector<vector<string>> files){
 
         char fname_char[fname.length() + 1];
         strcpy(fname_char, fname.c_str());
-        fname_char[fname.length()] = '\0';
 
         if (mkdir(fname_char, 0777) == -1)
             cerr << "Error :  " << strerror(errno) << endl;
-        else cout << "Directory created";
+        // else cout << "Directory created";
 
     }
 
     // takes int and creates a file with it inside each replicant
     // the content of the files is n, 6, or 8 
 
-    cout << "directories: ";   
-    print_vector(directories);
+    // cout << "directories: ";   
+    // print_vector(directories);
 
     for (int row = 1; row < files.size(); row++){
         string file_int = files.at(row).at(0); 
@@ -93,14 +94,11 @@ void create(vector<vector<string>> files){
     }
 }
 
-int main() {  
-    // string num = "1";  // test code fo create_filename func.
-    // cout << "filename: " << create_filename(num) << endl;
-
-    ifstream f("06.11.2021_DG5248_GFP_OLLAS_MSP.csv");
-
-    vector<vector<string>> csv;
-    vector<string> directories;
+vector<vector<string>> file_to_vector(string file) {
+    /* takes in a string pointing to a csv file 
+        that is then put into a 2d vector and returned */
+    ifstream f(file);
+    vector<vector<string>> v;
 
     if(f.is_open()) {
         string line, val;
@@ -118,7 +116,7 @@ int main() {
                     n.push_back(val);
                 }
             }
-            csv.push_back(n);
+            v.push_back(n);
             // cout << line << "\n";
         }
         f.close(); // file has been read, can be closed
@@ -126,11 +124,99 @@ int main() {
         cout << "file did not open" << endl;
     }
 
-    // for(int i = 0; i < csv.size(); i++){  /* prints out all values in csv file */
-    //     print_vector(csv.at(i));
-    // }
+    return v;
+}
+
+void create_directory(string s){
+    // cout << "dirname: " << s;
+    char dir[s.length() + 1];
+    strcpy(dir, s.c_str());
+
+    if (mkdir(dir, 0777) == -1)
+        cerr << "Error :  " << strerror(errno) << endl;
+    // else cout << "Directory created\n";
+}
+
+void sort_files(vector<vector<string>> v){
+
+    vector<string> directories;
+
+    for (int i = 1; i <= 4; i++){
+        string fname = v.at(0).at(i);
+        directories.push_back(fname);
+
+        create_directory(fname + "/N2");
+        create_directory(fname + "/SYGL-1_OLLAS_DG5136");
+        create_directory(fname + "/SYGL-1_OLLAS_DG5249");
+
+    }
+
+    for (int row = 1; row < v.size(); row++){
+        string file_int = v.at(row).at(0); 
+        for (int dir = 0; dir < directories.size(); dir++){
+            string file_content = v.at(row).at(dir + 1);
+            if (!file_content.empty()) {
+                // then file exists:
+                string filename = directories.at(dir) + "/";
+                string prefix = filename; // holds on to replicate_#/
+                string suffix = create_filename(file_int);
+                filename = filename + suffix;
+
+                char char_filename[filename.length()+1];
+                    //    char fname_char[fname.length() + 1];
+                strcpy(char_filename, filename.c_str());
+
+                // change filename to move it. 
+                string new_filename;
+
+                if(!file_content.compare("n")) { 
+                    // set folder name to replicate_#/N2/
+                    new_filename = prefix + "N2/" + suffix; 
+                    char char_new_filename[filename.length()+new_filename.length()];
+                    strcpy(char_new_filename, new_filename.c_str());
+
+                    if(rename(char_filename, char_new_filename) < 0){
+                        cout << strerror(errno) << endl;
+                    }
+                } else if(!file_content.compare("6")) {
+                    // set folder name to replicate_#/N2/
+                    new_filename = prefix + "SYGL-1_OLLAS_DG5136/" + suffix;
+                    char char_new_filename[filename.length()+new_filename.length()];
+                    strcpy(char_new_filename, new_filename.c_str());
+
+                    if(rename(char_filename, char_new_filename) < 0){
+                    cout << strerror(errno) << endl;
+                    }
+                } else if (!file_content.compare("8")) {
+                    // set folder name to replicate_#/N2/
+                    new_filename = prefix + "SYGL-1_OLLAS_DG5249/" + suffix;
+                    char char_new_filename[filename.length()+new_filename.length()];
+                    strcpy(char_new_filename, new_filename.c_str());
+
+                    if(rename(char_filename, char_new_filename) < 0){
+                        cout << strerror(errno) << endl;
+                    }
+                } else {
+                    // do nothing 
+                }
+            }
+        }
+    }
+
+}
+
+int main() {  
+    // string num = "1";  // test code fo create_filename func.
+    // cout << "filename: " << create_filename(num) << endl;
+
+    vector<vector<string>> csv;
+    csv = file_to_vector("06.11.2021_DG5248_GFP_OLLAS_MSP.csv");
     
-    create(csv);
+    create(csv); /* creates fake directories and 
+            correspoding files in them to organize*/
+
+    sort_files(csv);
+ 
 
     return 0;
 }
